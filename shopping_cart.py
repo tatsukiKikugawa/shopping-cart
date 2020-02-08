e518db0891f80f0ec808edbef2cf123891c09d7a
tatsukiKikugawa <tk802@georgetown.edu>
2020-02-08 17:45:52 -0500

    Separate info input and output receipts

--- shopping_cart.py
+++ shopping_cart.py
@@ -28,22 +28,30 @@ products = [
 #
 
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product indentifier: ") #< "9" (string)
     if selected_id == "DONE" or selected_id == "Done" or selected_id == "done":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0] #< to convert class list to class dict
-        total_price = total_price + matching_product["price"]
-        print("SELECTED_PRODUCT: " + matching_product["name"] + " ($" + str(matching_product["price"]) + ")")
+        selected_ids.append(selected_id)
 
 
 
 #
 #INFO DISPLAY / OUTPUT
 #
+
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0] #< to convert class list to class dict
+    total_price = total_price + matching_product["price"]
+    print("SELECTED_PRODUCT: " + matching_product["name"] + " ($" + str(matching_product["price"]) + ")")
+
+
+
+
 
 
 print("TOTAL PRICE: " + str(total_price))
